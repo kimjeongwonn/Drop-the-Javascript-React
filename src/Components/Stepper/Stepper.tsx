9a5a5cff65d2e98c3a18fc898f68e340bd49f6ae
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-17 10:10:44 +0900

    feat: Stepper 컴포넌트 작성
    - 값이 변경되고 Blur가 될 때 실제로 변경됨
    - step 단위로 값이 변경하고 직접 수정해도 step단위에 맞춰 근사값으로 변경됨

---
+++ src/Components/Stepper/Stepper.tsx
@@ -0,0 +1,71 @@
+import React, { ReactElement, useCallback, useEffect, useState } from 'react';
+import Button from '../Button/Button';
+import styles from './Stepper.module.scss';
+import minusIconSrc from '../../Assets/Image/minus_icon.svg';
+import plusIconSrc from '../../Assets/Image/plus_icon.svg';
+
+interface Props {
+  onChange: (newValue: number) => void;
+  valueState: number;
+  step?: number;
+  min?: number;
+  max?: number;
+  label?: string;
+}
+
+export default function Stepper({
+  onChange,
+  valueState,
+  step,
+  min,
+  max,
+  label
+}: Props): ReactElement {
+  const [inputValue, setInputValue] = useState(valueState);
+
+  function withStepCount(num: number): number {
+    const valueDiv = num % step;
+    const offset = valueDiv >= step / 2 ? -(step - valueDiv) : valueDiv;
+    return num - offset;
+  }
+
+  const changeValue = useCallback(
+    (input?: number) => {
+      let newValue = withStepCount(input ?? inputValue);
+      newValue = newValue > max ? max : newValue < min ? min : newValue;
+      setInputValue(newValue);
+      onChange(newValue);
+    },
+    [step, min, max, inputValue, onChange]
+  );
+
+  useEffect(() => {
+    changeValue(valueState);
+  }, [valueState]);
+
+  return (
+    <div className={styles.container}>
+      <div>{label}</div>
+      <div className={styles.stepper}>
+        <Button onClick={() => changeValue(inputValue - step)} size='sm' icon={minusIconSrc} />
+        <input
+          onBlur={() => changeValue()}
+          onChange={e => {
+            setInputValue(+e.target.value);
+          }}
+          value={inputValue}
+          className={styles.input}
+          size={3}
+        />
+        <Button onClick={() => changeValue(inputValue + step)} size='sm' icon={plusIconSrc} />
+      </div>
+    </div>
+  );
+}
+
+Stepper.defaultProps = {
+  min: 0,
+  max: Infinity,
+  step: 1,
+  label: ''
+};
