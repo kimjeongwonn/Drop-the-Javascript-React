1bdab41524e53a7c89d4545fcbe7c96fd90e3d7b
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-18 15:59:49 +0900

    feat: Slider 컴포넌트를 사용해 불륨을 조절하는 기능 추가 #1
    - AudioContext를 통해 받은 Gain 노드의 Ref를 통해 value를 조작하는 핸들러 전달하여 구현

--- src/Containers/Controller/Controller.tsx
+++ src/Containers/Controller/Controller.tsx
@@ -1,18 +1,19 @@
 import React, { ReactElement, useCallback } from 'react';
-import playIconSrc from '../../Assets/Image/play_icon.svg';
-import stopIconSrc from '../../Assets/Image/stop_icon.svg';
-import saveIconSrc from '../../Assets/Image/save_icon.svg';
 import clearIconSrc from '../../Assets/Image/clear_icon.svg';
 import loadIconSrc from '../../Assets/Image/load_icon.svg';
+import playIconSrc from '../../Assets/Image/play_icon.svg';
+import saveIconSrc from '../../Assets/Image/save_icon.svg';
+import stopIconSrc from '../../Assets/Image/stop_icon.svg';
 import Button from '../../Components/Button/Button';
+import Slider from '../../Components/Slider/Slider';
 import Stepper from '../../Components/Stepper/Stepper';
+import { useAudio } from '../../Contexts/AudioContext';
 import { useMusic } from '../../Contexts/MusicContext';
 import styles from './Controller.module.scss';
-import { usePage } from '../../Contexts/PageContext';
 
 export default function Controller(): ReactElement {
   const { setPlaying, playing, beat, bpm, setBpm, setBeat, setMusic, music } = useMusic();
-  const { setCurrentPage } = usePage();
+  const { audioContextGainRef } = useAudio();
 
   const togglePlaying = useCallback(() => {
     setPlaying(!playing);
@@ -30,6 +31,13 @@ export default function Controller(): ReactElement {
         <Stepper valueState={bpm} onChange={setBpm} step={10} min={100} max={500} label='BPM' />
       </div>
       <div className={styles.menu}>
+        <Slider
+          initialValue={0.5}
+          min={0}
+          max={1}
+          step={0.01}
+          onChange={e => (audioContextGainRef.current.gain.value = +e.target.value)}
+        />
         <Button icon={saveIconSrc} size='md' />
         <Button icon={loadIconSrc} size='md' />
         <Button onClick={clearMusic} icon={clearIconSrc} size='md' />
