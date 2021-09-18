73ce949f203dc5002a5dfa83760114e70bf56b40
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-18 15:57:33 +0900

    feat: Slider 컴포넌트 작성
    - 디자인만 커스텀한 range input과 같은 기능
    - min, max, value, onChange를 받아서 작동

---
+++ src/Components/Slider/Slider.tsx
@@ -0,0 +1,34 @@
+import React, { ReactElement, useCallback, useState } from 'react';
+import styles from './Slider.module.scss';
+
+interface Props {
+  max?: number;
+  min?: number;
+  initialValue: number;
+  step?: number;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+export default function Slider({ onChange, initialValue, ...args }: Props): ReactElement {
+  const [rangeValue, setRangeValue] = useState<number>(initialValue);
+  const onChangeHandler = useCallback<React.ChangeEventHandler<HTMLInputElement>>(e => {
+    setRangeValue(+e.target.value);
+    onChange(e);
+  }, []);
+
+  return (
+    <input
+      value={rangeValue}
+      className={styles.slider}
+      type='range'
+      {...args}
+      onChange={onChangeHandler}
+    ></input>
+  );
+}
+
+Slider.defaultProps = {
+  max: 100,
+  min: 0,
+  step: 1
+};
