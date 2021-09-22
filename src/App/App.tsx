7dc97e2c0b9b5e52494c4f281a531798830d0708
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-22 18:25:36 +0900

    feat: 재생중일때는 AudioVisualizer를 통해 애니메이션 출력 solved #5

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,16 @@
 import React, { ReactElement, useEffect } from 'react';
+import AudioVisualizer from '../Components/AudioVisualizer/AudioVisualizer';
 import Logo from '../Components/Logo/Logo';
 import Controller from '../Containers/Controller/Controller';
 import Panel from '../Containers/Panel/Panel';
 import { useAudio } from '../Contexts/AudioContext';
+import { useMusic } from '../Contexts/MusicContext';
+import { usePage } from '../Contexts/PageContext';
 
 export default function App(): ReactElement {
-  const { audioContextRef } = useAudio();
+  const { audioAnalyserRef, audioContextRef } = useAudio();
+  const { viewHeight, viewWidth } = usePage();
+  const { playing } = useMusic();
 
   useEffect(() => {
     window.addEventListener('click', function audioContextInit() {
@@ -19,6 +24,9 @@ export default function App(): ReactElement {
       <Logo />
       <Panel />
       <Controller />
+      {playing ? (
+        <AudioVisualizer analyserRef={audioAnalyserRef} width={viewWidth} height={viewHeight} />
+      ) : null}
     </>
   );
 }
