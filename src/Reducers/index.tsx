3e4feca12ee65f79dd921d6747ff239d508a9617
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-25 15:13:16 +0900

    feat: store 설정
    - reducer에 musicReducer와 viewReducer추가

--- src/Reducers/index.tsx
+++ src/Reducers/index.tsx
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import musicReducer from './musicSlice';
+import viewReducer from './viewSlice';
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    music: musicReducer,
+    view: viewReducer
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
