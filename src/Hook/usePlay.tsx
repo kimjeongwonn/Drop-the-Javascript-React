2f972698be8bb64999554d0c973d02b9b2706915
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-26 20:14:01 +0900

    refactor: usePlay hook을 Redux를 사용하도록 변경
    - audioContext에서 ref로 받은 인스턴스들을 바로 받는 방식으로 변경

--- src/Hook/usePlay.tsx
+++ src/Hook/usePlay.tsx
@@ -1,32 +1,35 @@
 import { useEffect, useRef, useState } from 'react';
 import { useAudio } from '../Contexts/AudioContext';
-import { InstType, useMusic } from '../Contexts/MusicContext';
+import { InstNameUnion } from '../Data/instData';
+import { MusicState } from '../Reducers/initialStore';
 
-export default function usePlay(): number {
+export default function usePlay(
+  music: MusicState['music'],
+  playing: boolean,
+  beat: number,
+  bpm: number
+): number {
   const timerId = useRef<number>();
-  const { playing, beat, bpm, music } = useMusic();
-  const { audioContextRef, instDataRef, audioContextGainRef } = useAudio();
+  const { audioContext, audioContextGain, instAudioBuffers } = useAudio();
   const [playingCol, setPlayingCol] = useState<number>(0);
 
   function playAudio(col: number) {
-    const playingInst: (keyof InstType)[] = [];
-    const ctx = audioContextRef.current;
+    const playingInst: InstNameUnion[] = [];
 
     music.forEach(row => {
       if (row.show && row.notes[col]) {
-        playingInst.push(row.inst);
+        playingInst.push(row.instName);
       }
     });
 
     playingInst.forEach(inst => {
-      const insts = instDataRef.current;
-      const node = ctx.createBufferSource();
-      node.buffer = insts[inst];
-      node.connect(audioContextGainRef.current);
+      const node = audioContext.createBufferSource();
+      node.buffer = instAudioBuffers[inst];
+      node.connect(audioContextGain);
       node.start();
       node.onended = function () {
         this.stop();
-        this.disconnect(audioContextGainRef.current);
+        this.disconnect(audioContextGain);
       };
     });
   }
