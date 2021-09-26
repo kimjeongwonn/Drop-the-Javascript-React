a1506ec70b23e84e720e7ffa8f200f06fb5d7a6f
kimjeongwon <kimjeongwonn@gmail.com>
2021-09-26 20:35:08 +0900

    fix: store보다 App 컴포넌트가 먼저 import되면 발생하는 에러 해결
    - store가 먼저 import되야 정상적으로 application이 작동

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { store } from './Reducers';
 import App from './App/App';
 import AudioProvider from './Contexts/AudioContext';
-import { store } from './Reducers';
 import './Styles/global.scss';
 
 ReactDOM.render(
